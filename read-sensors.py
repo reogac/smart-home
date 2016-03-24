1e45a7444d5e1517266c116f3f6583da8b1f4acf
Demo User <debian@beaglebone.localdomain>
2016-03-24 02:10:49 +0000

    add sensor reading script

---
+++ read-sensors.py
@@ -0,0 +1,69 @@
+import threading
+import time
+import serial
+import Queue
+from datetime import datetime
+PROBING_INTERVAL = 1 #reading at 1sec interval. It should be not to small
+SAVING_INTERVAL_NUM = 5
+FW_KEY = "(02)"
+class ReaderWriter(threading.Thread):
+    def __init__(self, data, key):
+        self.port = serial.Serial(
+                    port='/dev/ttyO2',
+                    baudrate=9600, timeout=0,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS
+                    )
+        self.port.flush() #clearing buffer
+        print self.port.isOpen()
+        self.alive = True
+        self.queue = data
+        self.key = key
+	threading.Thread.__init__(self)
+
+    def run(self):
+        while (self.alive):
+            self.port.write(self.key)
+            print "write"
+            bf = ""
+            while self.port.inWaiting():
+                bf += self.port.read()
+	    if (len(bf) == 32) :
+                print bf
+                self.queue.put(bf)
+            elif len(bf)>0:
+                print "got this: " + bf
+            time.sleep(PROBING_INTERVAL)
+
+        self.port.close()
+
+    def set_stop(self):
+	print "kill it"
+        self.alive = False
+ 
+def parse_data(data):
+    sensors={}
+    try:
+        sensors["dust"] = 0.01*int(data[10:15])
+        sensors["temp"] = 0.1*int(data[17:21])
+        sensors["humidity"] = int(data[23:26])
+        sensors["light"] = int(data[28:31])
+        sensors["time"] = str(datetime.now())
+        print sensors
+    except ValueError as e:
+        print e
+
+
+data = Queue.Queue()
+rw = ReaderWriter(data, FW_KEY)
+rw.start()
+
+while True:
+    try:
+        msg = data.get()
+        sensors = parse_data(msg)
+    except (KeyboardInterrupt, SystemExit):
+        rw.set_stop()
+        break
+
