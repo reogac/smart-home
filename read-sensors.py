7871ac24b4a8a779b27ca261813736d3bf4b8122
Thai Quang Tung <tqtung@gmail.com>
2016-04-19 13:57:04 +0900

    create predictor class

--- read-sensors.py
+++ read-sensors.py
@@ -3,25 +3,35 @@ import time
 import serial
 import Queue
 from datetime import datetime
+import numpy as np
+
 PROBING_INTERVAL = 1 #reading at 1sec interval. It should be not to small
 SAVING_INTERVAL_NUM = 5
 FW_KEY = "(02)"
+NUM_SENSORS = 5
+AMBIENT_SENSORS = 1
 
-class CommandSender(Thread):
+class MyThread(Thread, my_name):
+    def __init__(self, name):
+        self.alive = True
+        self.my_name = my_name
+        Thread.__init__(self)
+
+    def set_kill(self):
+        print "kill " + self.my_name + " thread!"
+        self.alive = False
+
+class CommandSender(MyThread):
     def __init__(self, command):
         self.command = command
-        self.alive = True
-        Thread.__init__(self)
+        MyThread.__init__(self)
 
     def run(self):
         while self.alive:
-            self.command.put(1)
+            self.command.put(AMBIENT_SENSORS)
             time.sleep(PROBING_INTERVAL)
-    def set_kill(self):
-        print "kill command sender thread"
-        self.alive = False
 
-class ReaderWriter(Thread):
+class ReaderWriter(MyThread):
     def __init__(self, command, data):
         self.port = serial.Serial(
                     port='/dev/ttyO2',
@@ -31,17 +41,16 @@ class ReaderWriter(Thread):
                     bytesize=serial.EIGHTBITS
                     )
         print self.port.isOpen()
-        self.alive = True
         self.queue = data
         self.command = command
-        Thread.__init__(self)
+        MyThread.__init__(self)
 
     def run(self):
         self.port.flush() #clearing input buffer
         bf = ""
         while (self.alive):
             c = self.command.get()
-            if c==1:
+            if c==AMBIENT_SENSORS:
                 #Tell the port that I want more data
                 self.port.write(FW_KEY)
                 print "write"
@@ -57,42 +66,48 @@ class ReaderWriter(Thread):
 
         self.port.close()
 
-    def set_kill(self):
-        print "kill reader-writer thread"
-        self.alive = False
- 
-def parse_data(data):
-    sensors={}
-    try:
-        sensors["dust"] = 0.01*int(data[10:15])
-        sensors["temp"] = 0.1*int(data[17:21])
-        sensors["humidity"] = int(data[23:26])
-        sensors["light"] = int(data[28:31])
-        sensors["time"] = str(datetime.now())
-        print sensors
-    except ValueError as e:
-        print e
+
+class Predictor(MyThread):
+    def __init__(self):
+        self.data = Queue.Queue(100)
+        command = Queue.Queue(100)
+        self.command_sender = CommandSender(command)
+        self.port_reader_writer = ReaderWriter(command, self.data)
+        MyThread.__init(self)
+
+    def parse_data(data):
+        sensors={}
+        try:
+            sensors["dust"] = 0.01*int(data[10:15])
+            sensors["temp"] = 0.1*int(data[17:21])
+            sensors["humidity"] = int(data[23:26])
+            sensors["light"] = int(data[28:31])
+            sensors["time"] = str(datetime.now())
+            print sensors
+        except ValueError as e:
+            print e
+    def run(self):
+        self.command_sender.start()
+        self.port_reader_writer.start()
+
+        while self.alive:
+            try:
+                msg = self.data.get(timeout=1)
+                sensors = self.parse_data(msg)
+
+            except (KeyboardInterrupt, SystemExit):
+                print "interupted"
+                self.command_sender.set_kill()
+                time.sleep(1)
+                self.port_reader_writer.set_kill()
+                break
+            except Queue.Empty as e:
+                print "pass the empty queue exception"
+                pass
 
 
-data = Queue.Queue(100)
-command = Queue.Queue(100)
-cs = CommandSender(command)
-rw = ReaderWriter(command, data)
-rw.start()
-cs.start()
+pred = Predictor()
+pred.start()
+pred.join()
 
-
-while True:
-    try:
-        msg = data.get(timeout=1)       
-        sensors = parse_data(msg)
-    except (KeyboardInterrupt, SystemExit):
-        print "interupted"
-        rw.set_kill()
-        time.sleep(1)
-        cs.set_kill()
-        break
-    except Queue.Empty as e:
-        print "pass the empty queue exception"
-        pass
 
